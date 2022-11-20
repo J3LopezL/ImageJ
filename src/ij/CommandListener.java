7d636ebc0a848dfd8ed7ed0c057aa3138c3db58d
mgaitans <m.gaitans@uniandes.edu.co>
2022-11-20 18:25:13 -0500

    ajuste error

--- src/ij/CommandListener.java
+++ src/ij/CommandListener.java
@@ -1,16 +1,16 @@
-package ij;
+import ij.util.StringSorter;
 
-	/** Plugins that implement this interface are notified when ImageJ
+/** Plugins that implement this interface are notified when ImageJ
 		is about to run a menu command. There is an example plugin at
 		http://imagej.nih.gov/ij/plugins/download/misc/Command_Listener.java
 	*/
 	public interface CommandListener {
 
-	/*	The method is called when ImageJ is about to run a menu command, 
-		where 'command' is the name of the command. Return this string 
-		and ImageJ will run the command, return a different command name
-		and ImageJ will run that command, or return null to not run a command.
-	*/
-	public String commandExecuting(String command);
+		/*	The method is called when ImageJ is about to run a menu command, 
+			where 'command' is the name of the command. Return this string 
+			and ImageJ will run the command, return a different command name
+			and ImageJ will run that command, or return null to not run a command.
+		*/
+		public StringSorter commandExecuting(StringSorter command);
 
-}
+	}
